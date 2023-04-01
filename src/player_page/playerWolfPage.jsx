dc06b84d23e8b456e0af543b184345feceefe845
nhh1603 <69860012+nhh1603@users.noreply.github.com>
2023-04-02 00:36:46 +0200

    QR Code

--- src/player_page/playerWolfPage.jsx
+++ src/player_page/playerWolfPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QRCode from "react-qr-code";
 import { doc, updateDoc, onSnapshot, collection, query, getDoc } from "firebase/firestore";
 import db from '../firebase';
 
@@ -41,45 +42,45 @@ class PlayerWolfPage extends Component {
         });
     }
 
-    // handleBack = async () => {
-    //     const playerRef = doc(db, "players", this.state._id);
-    //     const playerSnap = await getDoc(playerRef);
-    //     if (playerSnap.data().gameRoom !== null) {
-    //         const gameRoomRef = doc(db, "gamerooms", playerSnap.data().gameRoom.id);
-    //         const gameRoomSnap = await getDoc(gameRoomRef);
-    //         var listPlayersInRoom = gameRoomSnap.data().listPlayers;
-    //         listPlayersInRoom = listPlayersInRoom.filter(playerId => playerId !== playerSnap.id);
-    //         await updateDoc(gameRoomRef, {
-    //             listPlayers: listPlayersInRoom
-    //         });
-    //         await updateDoc(playerRef, {
-    //             gameRoom: null,
-    //             stateInGame: ""
-    //         });
-    //     }
-    // }
+    handleBack = async () => {
+        const playerRef = doc(db, "players", this.state._id);
+        const playerSnap = await getDoc(playerRef);
+        if (playerSnap.data().gameRoom !== null) {
+            const gameRoomRef = doc(db, "gamerooms", playerSnap.data().gameRoom.id);
+            const gameRoomSnap = await getDoc(gameRoomRef);
+            var listPlayersInRoom = gameRoomSnap.data().listPlayers;
+            listPlayersInRoom = listPlayersInRoom.filter(playerId => playerId !== playerSnap.id);
+            await updateDoc(gameRoomRef, {
+                listPlayers: listPlayersInRoom
+            });
+            await updateDoc(playerRef, {
+                gameRoom: null,
+                stateInGame: ""
+            });
+        }
+    }
 
-    // handleLogOut = async () => {
-    //     const playerRef = doc(db, "players", this.state._id);
-    //     const playerSnap = await getDoc(playerRef);
-    //     if (playerSnap.data().gameRoom !== null) {
-    //         const gameRoomRef = doc(db, "gamerooms", playerSnap.data().gameRoom.id);
-    //         const gameRoomSnap = await getDoc(gameRoomRef);
-    //         var listPlayersInRoom = gameRoomSnap.data().listPlayers;
-    //         listPlayersInRoom = listPlayersInRoom.filter(playerId => playerId !== playerSnap.id);
-    //         await updateDoc(gameRoomRef, {
-    //             listPlayers: listPlayersInRoom
-    //         });
-    //         await updateDoc(playerRef, {
-    //             gameRoom: null
-    //         });
-    //     }
-    //     // Set the "online" field of the current player to false
-    //     await updateDoc(playerRef, {
-    //         online: false
-    //     });
-    //     window.location = '/';
-    // }
+    handleLogOut = async () => {
+        const playerRef = doc(db, "players", this.state._id);
+        const playerSnap = await getDoc(playerRef);
+        if (playerSnap.data().gameRoom !== null) {
+            const gameRoomRef = doc(db, "gamerooms", playerSnap.data().gameRoom.id);
+            const gameRoomSnap = await getDoc(gameRoomRef);
+            var listPlayersInRoom = gameRoomSnap.data().listPlayers;
+            listPlayersInRoom = listPlayersInRoom.filter(playerId => playerId !== playerSnap.id);
+            await updateDoc(gameRoomRef, {
+                listPlayers: listPlayersInRoom
+            });
+            await updateDoc(playerRef, {
+                gameRoom: null
+            });
+        }
+        // Set the "online" field of the current player to false
+        await updateDoc(playerRef, {
+            online: false
+        });
+        window.location = '/';
+    }
 
 
     render() {
@@ -90,8 +91,16 @@ class PlayerWolfPage extends Component {
                     <h2 className="Playing">
                         Welcome to the game of WOLF !!
                     </h2>
-                    {/* <button id="logOutButton" onClick={this.handleBack}>Back</button>
-                    <button id="logOutButton" onClick={this.handleLogOut}>Log out</button> */}
+                    <div style={{ height: "auto", margin: "0 auto", maxWidth: 128, width: "100%" }}>
+                        <QRCode
+                        size={256}
+                        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                        value="https://www.google.com"
+                        viewBox={`0 0 256 256`}
+                        />
+                    </div>
+                    <button id="logOutButton" onClick={this.handleBack}>Back</button>
+                    <button id="logOutButton" onClick={this.handleLogOut}>Log out</button>
                 </div>
             </>
         );
